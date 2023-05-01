1ecd3e1f386b7ee71c5d34260e48b600c73aab2a
Leonardo Bertele Tosin <leob.tosin@hotmail.com>
2023-05-01 13:05:06 -0300

    feat: Create the DI package

---
+++ src/Frankfurter.API.Client.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Frankfurter.API.Client.Configuration;
+using Frankfurter.API.Client.Infraestructure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Frankfurter.API.Client.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddFrankfurterApiClient(this IServiceCollection services)
+        {
+            services.AddTransient<IFrankfurterClientHttpClient, FrankfurterClientHttpClient>();
+
+            services.AddTransient<IFrankfurterClient>(x =>
+                new FrankfurterClient(x.GetRequiredService<IFrankfurterClientHttpClient>()));
+
+            return services;
+        }
+
+        public static IServiceCollection AddFrankfurterApiClient(this IServiceCollection services, string baseUrl)
+        {
+            services.AddTransient<IFrankfurterClientHttpClient>(_ =>
+                new FrankfurterClientHttpClient(baseUrl));
+
+            services.AddTransient<IFrankfurterClient>(x =>
+                new FrankfurterClient(x.GetRequiredService<IFrankfurterClientHttpClient>()));
+
+            return services;
+        }
+
+        public static IServiceCollection AddFrankfurterApiClient(this IServiceCollection services, FrankfurterClientConfiguration configs)
+        {
+            services.AddTransient<IFrankfurterClientHttpClient>(_ =>
+                new FrankfurterClientHttpClient(configs));
+
+            services.AddTransient<IFrankfurterClient>(x =>
+                new FrankfurterClient(x.GetRequiredService<IFrankfurterClientHttpClient>()));
+
+            return services;
+        }
+    }
+}
