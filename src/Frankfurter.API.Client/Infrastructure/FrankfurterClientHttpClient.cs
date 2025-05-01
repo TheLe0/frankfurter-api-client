53a2b489af5baaff62d143d5c0af4d691e9820f1
Leonardo B. Tosin <leob.tosin@hotmail.com>
2025-05-01 18:51:50 -0300

    refact: Change MaxTimeout to Timeout

--- src/Frankfurter.API.Client/Infrastructure/FrankfurterClientHttpClient.cs
+++ src/Frankfurter.API.Client/Infrastructure/FrankfurterClientHttpClient.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Frankfurter.API.Client.Configuration;
 using RestSharp;
 
@@ -42,7 +43,7 @@ namespace Frankfurter.API.Client.Infrastructure
             return new RestClientOptions(_configuration.BaseApiUrl)
             {
                 ThrowOnAnyError = _configuration.ThrowOnAnyError,
-                MaxTimeout = _configuration.MaxTimeout
+                Timeout = TimeSpan.FromMilliseconds(_configuration.MaxTimeout)
             };
         }
     }
